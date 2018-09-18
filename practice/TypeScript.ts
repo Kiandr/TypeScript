b611e8330fa783b2a94c1e2f9a4eef21983dfdc6
Kian Davoudi-Rad <kiandr@KIAN.hitronhub.home>
2018-09-17 21:05:51 -0700

    adding altest changes

---
+++ practice/TypeScript.ts
@@ -0,0 +1,12 @@
+/*
+// descrete type
+function Idetity (arg: number): number{ return arg;}
+// Any type
+function Idetity (arg: any): any {return arg;}
+// generic type
+function Idetity<T> (arg: T):T{return arg;}
+// consume generic type 
+let output = Idetity<String>("MyString");
+// arrowed generic function declaration 
+function Idetity<T> (arg : T)T{return arg;}
+*/
